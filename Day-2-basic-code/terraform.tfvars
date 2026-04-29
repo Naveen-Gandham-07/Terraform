33787611b60096c54de43fc93a869e235faf1b2d
Naveen <naveenkumar7780488@gmail.com>
2026-04-29 18:22:42 +0530

    gitignore

---
+++ Day-2-basic-code/terraform.tfvars
@@ -0,0 +1,3 @@
+ami = "ami-0f559c3642608c138"
+type = "t3.micro"
+key = "3-tier-project"
---
+++ Day-2-basic-code/veriables.tf
@@ -0,0 +1,17 @@
+variable "ami" {
+    type = string
+    default = "ami-0f559c3642608c138"
+  
+}
+
+variable "type" {
+    type = string
+    default = "t3.nano"
+  
+}
+
+variable "key" {
+    type = string
+    default = "3-tier-project"
+  
+}
