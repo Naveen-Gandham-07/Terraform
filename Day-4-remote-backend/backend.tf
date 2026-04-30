5ed93ea41d4a48c422350d44cea6231f73fe41bc
Naveen <naveenkumar7780488@gmail.com>
2026-04-30 13:52:29 +0530

    adding dynamodb

--- Day-4-remote-backend/backend.tf
+++ Day-4-remote-backend/backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "naveen-test-buc"
     key    = "terraform.tfstate"
     region = "ap-south-1"
+    dynamodb_table = "terraform-state-lock-dynamodb"
+    encrypt = true
   }
 }
