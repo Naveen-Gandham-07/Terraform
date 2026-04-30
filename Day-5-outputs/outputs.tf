e8cf21d90422f773620ef34aec751bf394f3d034
Naveen <naveenkumar7780488@gmail.com>
2026-04-30 14:30:39 +0530

    adding outputs

---
+++ Day-5-outputs/outputs.tf
@@ -0,0 +1,14 @@
+output "ip" {
+    value = aws_instance.test.public_ip
+  
+}
+
+output "privateIp" {
+    value = aws_instance.test.private_ip
+  
+}
+
+output "Az" {
+    value = aws_instance.test.availability_zone
+  
+}
