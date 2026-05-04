f020713bc87216499fa053e6e7ea78980fdb0a54
Naveen-Gandham-07 <naveengandham0706@gmail.com>
2026-05-04 14:25:51 +0530

    Update main.tf

--- Day-6-Lambda/main.tf
+++ Day-6-Lambda/main.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "my_lambda" {
   function_name = "simple_lambda"
 
   role    = "arn:aws:iam::641867104819:role/lambda-exce"  # <-- replace
-  handler = "index.lambda_handler"
+  handler = "lambda_function.lambda_handler"
   runtime = "python3.9"
 
   filename         = "lambda_function.zip"
